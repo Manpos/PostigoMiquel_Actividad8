1a94e3f20d19a6d75083d1ed061d8301fef92cab
Manpos <miquelpostigo@gmail.com>
2016-11-30 00:21:20 +0100

    Save

--- PostigoMiquel_Actividad8/Source.cpp
+++ PostigoMiquel_Actividad8/Source.cpp
@@ -9,16 +9,45 @@ private:
 	int year;
 
 public:
-	Fecha();
-	~Fecha();
+	Fecha(int d, int m, int y);
+};
 
+Fecha::Fecha(int d, int m, int y){
+	day = d;
+	month = m;
+	year = y;
+}
+
+class Hora {
+private:
+	int hour;
+	int minutes;
+
+public:
+	Hora(int h, int m);
+};
+
+Hora::Hora(int h, int m) {
+	hour = h;
+	minutes = m;
+}
+
+
+
+class Agenda {
+private:
+	std::map < Fecha, std::map < Hora, std::string> > agnd;
+	std::map <Hora, std::string> evt;
+public:
+	void insertarEvento(const Fecha &fecha, const Hora &hora, const std::string &descripcion);
 
 };
 
-Fecha::Fecha()
-{
+void Agenda::insertarEvento(const Fecha &fecha, const Hora &hora, const std::string &descripcion) {
+	evt.emplace(descripcion, hora);
+	agnd.emplace(evt, fecha);
 }
 
-Fecha::~Fecha()
-{
+void main() {
+
 }
