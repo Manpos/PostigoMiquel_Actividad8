71436c74a91fdae5e71495cde08071fe376d0979
Manpos <miquelpostigo@gmail.com>
2016-11-29 18:58:21 +0100

    Add project files.

---
+++ PostigoMiquel_Actividad8/Source.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+#include <map>
+
+class Fecha {
+private:
+	int day;
+	int month;
+	int year;
+
+public:
+	Fecha();
+	~Fecha();
+
+
+};
+
+Fecha::Fecha()
+{
+}
+
+Fecha::~Fecha()
+{
+}
