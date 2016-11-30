edbc2d6430166b88f07bb059e9691132fa1d8485
Manpos <miquelpostigo@gmail.com>
2016-11-30 22:47:24 +0100

    Update 2

--- PostigoMiquel_Actividad8/Source.cpp
+++ PostigoMiquel_Actividad8/Source.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
 #include <map>
 
 class Fecha {
@@ -7,10 +8,45 @@ private:
 	int day;
 	int month;
 	int year;
-
 public:
 	Fecha(int d, int m, int y);
+	bool operator() (const Fecha& fch, const Fecha& fch2)
+	{
+		if (fch.year < fch2.year) {
+			if (fch.month < fch2.month) {
+				if (fch.day < fch2.day) {
+					return true;
+				}
+			}
+		}
+		else return false;
+	}
+	//bool operator < (const Fecha &fch);
+	//bool operator == (const Fecha &fch);
+
 };
+
+//inline bool operator< (const Fecha& fch, const Fecha& fch2) {
+//	if (fch.year < fch2.year) { 
+//		if (fch.month < fch2.month) { 
+//			if (fch.day < fch2.day) { 
+//				return true; 
+//			}
+//		} 
+//	}
+//	else return false;
+//}
+
+//bool Fecha::operator==(const Fecha &fch) {
+//	if ((*this).year == fch.year) {
+//		if ((*this).month == fch.month) {
+//			if ((*this).day == fch.day) {
+//				return true;
+//			}
+//		}
+//	}
+//	else return false;
+//}
 
 Fecha::Fecha(int d, int m, int y){
 	day = d;
@@ -36,18 +72,26 @@ Hora::Hora(int h, int m) {
 
 class Agenda {
 private:
-	std::map < Fecha, std::map < Hora, std::string> > agnd;
-	std::map <Hora, std::string> evt;
+	std::map <const Hora, const std::string> evt;
+	std::map <const Fecha, std::map <const Hora, const std::string>> agnd;
 public:
-	void insertarEvento(const Fecha &fecha, const Hora &hora, const std::string &descripcion);
-
+	Agenda();
+	void insertarEvento(const Fecha &fecha, const Hora &hora, const std::string& descripcion);
+	int GetSize() {	return agnd.size();	}
 };
 
-void Agenda::insertarEvento(const Fecha &fecha, const Hora &hora, const std::string &descripcion) {
-	evt.emplace(descripcion, hora);
-	agnd.emplace(evt, fecha);
+Agenda::Agenda(){}
+
+void Agenda::insertarEvento(const Fecha& fecha, const Hora& hora, const std::string& descripcion) {
+	evt.emplace(hora,descripcion);
+	agnd.emplace(fecha, evt);
+
 }
 
 void main() {
-
+	//Agenda agn;
+	const Fecha date(1,11,1997);
+	const Hora time(12, 50);
+	//agn.insertarEvento(date,time,"Things to do");
+	//std::cout << agn.GetSize();
 }
